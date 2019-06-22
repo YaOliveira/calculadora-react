39bfcce730046c912ae1b7644a2d232a1be22405
YaOliveira <magicsunshine_ya@msn.com>
2019-06-22 19:09:59 -0300

    reajuste da altura do h1 e edição do título

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.Fragment>
+  <Fragment>
     <h1>Calculadora</h1>
     <Calculator />
-  </React.Fragment>
+  </Fragment>
   , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
